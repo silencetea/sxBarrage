21356596b3ce1a8c6de9dc0268fb8531ddbfcf0f
xxsilence <silencetea@yeah.net>
2016-12-19 12:45:36 +0800

    connect the sever

---
+++ conn/conn.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+http.createServer( function (request, response) {
+	// 发送HTTP头部
+	// HTTP状态值：200 ：OK
+	// 内容类型："text/plain"
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	// 发送响应数据
+	response.write('helloworld');
+	response.end();
+}).listen(3000);
+
+console.log('success!')
